43c3996e6b7d55d0cb1849a90bdbc901d9967ace
Adarsh Gajbhhare <60750149+adarshplus@users.noreply.github.com>
2023-07-11 00:05:23 +0530

    Added code changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Navbar from './components/navbar'
 
 function App() {
+  const a = 10;
+  const b = 10;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {
+        a
+      }
+      < Navbar a = {a} b={b} />
     </div>
-  );
+  )
 }
 
 export default App;
